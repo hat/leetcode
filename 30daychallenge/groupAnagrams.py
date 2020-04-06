802935e25004130ed70d151409b7dccc7baa75ae
Tony Hendrick <hat@Tonys-MacBook-Air.local>
2020-04-06 13:00:23 -0700

    Day four and five added

---
+++ 30daychallenge/groupAnagrams.py
@@ -0,0 +1,36 @@
+# Given an array of strings, group anagrams together.
+
+# Example:
+
+# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# Output:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+
+# Note:
+
+#     All inputs will be in lowercase.
+#     The order of your output does not matter.
+
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: [str]) -> [[str]]:
+        all_groups = []
+        groups = {}
+        # create dictionary to store letters sorted and index is already in dict
+        # write to array
+        for word in strs:
+            if ''.join(sorted(word)) in groups:
+                groups[''.join(sorted(word))].append(word)
+            else:
+                groups[''.join(sorted(word))] = [word]
+        for group in groups:
+            all_groups.append(sorted(groups[group]))
+        return(all_groups)
+
+answer = Solution()
+print(answer.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
--- README.md
+++ README.md
@@ -8,4 +8,7 @@ Problems from https://leetcode.com/ to keep my programming skills sharp :)
 
 
 ## Arrays, Stacks, Queues
+
 * [Move Zeroes](https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3286/)
+* [Best Time to Buy and Sell Stock II](https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3287/)
+* [Group Anagrams](https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3288/)
